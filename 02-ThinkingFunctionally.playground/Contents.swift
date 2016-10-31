2a29545e8a6304eb0b78339f02c391b9e27419eb
大石弘一郎 <k_ooishi@waku-2.com>
2016-10-31 16:34:59 +0900

    僚艦と敵艦を追加

--- 02-ThinkingFunctionally.playground/Contents.swift
+++ 02-ThinkingFunctionally.playground/Contents.swift
@@ -1,11 +1,16 @@
 import Foundation
 
+// 自艦
+let shipPosition = Position(x: 0, y: 10)
+let ship = Ship(position: shipPosition, firingRange: 100, unsafeRange: 40)
 
-let position = Position(x: 0, y: 10)
-let ship = Ship(position: position, firingRange: 100, unsafeRange: 40)
+// 僚艦
+let friendlyShipPosition = Position(x: 100, y: 100)
+let friendlyShip = Ship(position: friendlyShipPosition, firingRange: 100, unsafeRange: 40)
 
+// 敵艦
+let targetShipPosition = Position(x: 0, y: 0)
+let targetShip = Ship(position: targetShipPosition, firingRange: 100, unsafeRange: 40)
 
-let friendly = Ship(position: position, firingRange: 100, unsafeRange: 40)
+let preview = draw(ship: ship, friendly: friendlyShip, target: targetShip)
 
-let preview = drawShip(ship: ship)
-//let preview = drawShip(ship: ship, friendly: friendly)
---
+++ 02-ThinkingFunctionally.playground/Sources/Draw.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+public func draw(ship: Ship, friendly: Ship, target: Ship) -> UIView {
+
+  let size = CGSize(width: 320, height: 320)
+  let view:UIView = UIView(frame: CGRect(origin: CGPoint.zero, size: size))
+  view.backgroundColor = UIColor(white: 0.9, alpha: 1.0)
+  
+  UIGraphicsBeginImageContextWithOptions(size, false, 0)
+
+  drawShip(size: size, ship: ship)
+  drawOtherShip(size: size, ship: friendly)
+  drawTargetShip(size: size, ship: target)
+  
+  // 上下の線を描写
+  drawLine(from: CGPoint(x: 0, y: size.height / 2), to: CGPoint(x: size.width, y: size.height / 2))
+  drawLine(from: CGPoint(x: size.width / 2, y: 0), to: CGPoint(x: size.width / 2, y: size.height))
+
+  view.layer.contents = UIGraphicsGetImageFromCurrentImageContext()?.cgImage
+  UIGraphicsEndImageContext()
+
+  return view
+}
+
+// 自分の船を描く
+func drawShip(size: CGSize, ship: Ship) {
+  let shipCenterPoint = CGPoint(x: size.width / 2 + CGFloat(ship.position.x), y: size.height / 2 + CGFloat(ship.position.y) * -1)
+  drawCircle(arcCenter: shipCenterPoint, radius: CGFloat(ship.firingRange), color: UIColor.green)
+  drawCircle(arcCenter: shipCenterPoint, radius: CGFloat(ship.unsafeRange), color: UIColor.white)
+  drawCircle(arcCenter: shipCenterPoint, radius: 2, color: UIColor.black)
+}
+
+// 味方の船を描く
+func drawOtherShip(size: CGSize, ship: Ship) {
+  let shipCenterPoint = CGPoint(x: size.width / 2 + CGFloat(ship.position.x), y: size.height / 2 + CGFloat(ship.position.y) * -1)
+  drawCircle(arcCenter: shipCenterPoint, radius: CGFloat(ship.unsafeRange), color: UIColor.white)
+  drawCircle(arcCenter: shipCenterPoint, radius: 2, color: UIColor.black)
+}
+
+// 目標の船を描く
+func drawTargetShip(size: CGSize, ship: Ship) {
+  let shipCenterPoint = CGPoint(x: size.width / 2 + CGFloat(ship.position.x), y: size.height / 2 + CGFloat(ship.position.y) * -1)
+  drawCircle(arcCenter: shipCenterPoint, radius: 4, color: UIColor.red)
+}
+
+// 直線を描く
+func drawLine(from: CGPoint, to: CGPoint) {
+  let path_line = UIBezierPath();
+  path_line.move(to: from);
+  path_line.addLine(to: to);
+  UIColor.black.setStroke();
+  path_line.stroke();
+}
+
+// 円を描く
+func drawCircle(arcCenter: CGPoint, radius: CGFloat, color: UIColor) {
+  let path = UIBezierPath(arcCenter: arcCenter, radius: radius, startAngle: 0, endAngle: CGFloat(M_PI)*2, clockwise: true)
+  UIColor.black.setStroke()
+  path.lineWidth = 2
+  color.setFill()
+  path.stroke()
+  path.fill()
+}
